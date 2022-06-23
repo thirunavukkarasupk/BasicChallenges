4866597ba65bf2de8d52a9961807fea6f8067c91
Thirunavukkarasu <38079914+thirunavukkarasupk@users.noreply.github.com>
2022-06-24 01:57:00 +0530

    Update cartusingmrf.js

--- cartusingmrf.js
+++ cartusingmrf.js
@@ -32,7 +32,7 @@ var rates = {
   var getDiscountPercent = productName => discounts.hasOwnProperty(productName) ? discounts[productName] : 0;
   var getTaxPercent = productName => taxes.hasOwnProperty(productName) ? taxes[productName] : 0;
   var getUnitPrice = itemName => rates.hasOwnProperty(itemName) ? rates[itemName] : 0;
-  var getLineItemPrice =  purchases => (getUnitPrice(purchases.item)*purchases.units) - ((getUnitPrice(purchases.item)*purchases.units)*(getDiscountPercent(purchases.item)/100)) + ((getUnitPrice(purchases.item)*purchases.units)*(getTaxPercent(purchases.item)/100));
+  var getLineItemPrice =  lineItem => (getUnitPrice(lineItem.item)*lineItem.units) - ((getUnitPrice(lineItem.item)*lineItem.units)*(getDiscountPercent(lineItem.item)/100)) + ((getUnitPrice(lineItem.item)*lineItem.units)*(getTaxPercent(lineItem.item)/100));
   var getSum = () => purchases.map(getLineItemPrice).reduce((totalPrice,currentElementPrice) =>totalPrice+currentElementPrice,0);   
 
   var main = function main() {
